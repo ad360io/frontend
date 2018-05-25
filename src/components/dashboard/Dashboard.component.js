af09dda2560480098fae90bd692915460272366b
yao tsai <a9898123u@gmail.com>
2018-05-24 17:42:48 -0700

    moved API request to start and end on dashboard lifecycle, will need another separate request for marketplace in future

--- src/components/dashboard/Dashboard.component.js
+++ src/components/dashboard/Dashboard.component.js
@@ -15,6 +15,11 @@ React Bootstrap Components
 import { Col, Row, Grid } from 'react-bootstrap';
 
 /*
+Networking
+*/
+import axios from 'axios';
+
+/*
 Custom Components
 */
 import Footer           from '../footer/Footer.component';
@@ -29,8 +34,23 @@ import DashboardCharts  from './DashboardCharts/DashboardCharts.component';
  */
 class Dashboard extends Component {
 
-    componentDidMount(){
-        document.title = "Qchain - Dashboard"
+    constructor(props){
+        super(props);
+        props.onStartLoadData();
+    }
+
+    componentDidMount() {
+        document.title = "Qchain - Dashboard";
+        
+        // Register data loading every 10 minutes.
+        const tenMinutes = 1000 * 60 * 10 ;
+        this.loadDataInterval = setInterval(this.props.onStartLoadData, tenMinutes);
+    }
+
+    componentWillUnmount() {
+        // Tidy up
+        clearInterval(this.loadDataInterval);
+        this.loadDataInterval = 0;
     }
 
     render() {
@@ -39,11 +59,14 @@ class Dashboard extends Component {
                 <Row>
                     <Col xs={12} lg={5} sm={8} className='dashboard-left'>
                         <DashboardWallet className='wallet-div'/>
-                        <StatsProvider   className='stats-div'/>
+                        <DashboardStats modeFilter={this.props.modeFilter} 
+                            currencyFilter={this.props.currencyFilter}  
+                            className='stats-div'/>
                     </Col>
 
                     <Col xs={12} lg={7} sm={4} className='dashboard-right'>
-                        <ChartsProvider />
+                        <DashboardCharts modeFilter={this.props.modeFilter}
+                            currencyFilter={this.props.currencyFilter} />
                     </Col>
                 </Row>
             </Grid>
@@ -58,15 +81,32 @@ const mapStateToProps = (state) => {
         currencyFilter  : state.MenuBarFilterReducer.currencyFilter,
     }
 }
-const StatsProvider = connect(
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onStartLoadData : () => {
+            dispatch((dispatch) => {
+                dispatch({type: 'FETCH_DATABASE_PENDING'})
+                axios.get(`${window.location.protocol}//${window.location.host}/api/db`)
+                    .then((response) => {
+                        dispatch({
+                            type: 'FETCH_DATABASE_FULFILLED',
+                            payload: response.data
+                        })
+                    })
+                    .catch((err) => {
+                        dispatch({
+                            type: 'FETCH_DATABASE_REJECTED',
+                            payload: err
+                        })
+                    })
+            })
+        }
+    }
+}
+
+
+export default connect(
     mapStateToProps,
-    {}
-)(DashboardStats)
-
-const ChartsProvider = connect(
-    mapStateToProps,
-    {}
-)(DashboardCharts)
-
-
-export default Dashboard;
+    mapDispatchToProps
+)(Dashboard);
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
-// Redux
+/*
+Redux
+*/
 import { applyMiddleware, createStore } from 'redux';
 import thunk                            from 'redux-thunk';
 import { createLogger }                 from 'redux-logger';
 
-// Reducers
+/*
+Reducer
+*/ 
 import RootReducer from '../reducers/RootReducer';
 
-// Networking
-import axios from 'axios';
 
 // Middleware
 const middleware = [ thunk ];
@@ -21,29 +23,5 @@ const store = createStore(
     applyMiddleware(...middleware)
 );
 
-// Core dispatcher
-var databaseDispatcher = () => {
-    store.dispatch((dispatch) => {
-        dispatch({type: 'FETCH_DATABASE_PENDING'})
-        axios.get(`${window.location.protocol}//${window.location.host}/api/db`)
-            .then((response) => {
-                dispatch({
-                    type: 'FETCH_DATABASE_FULFILLED',
-                    payload: response.data
-                })
-            })
-            .catch((err) => {
-                dispatch({
-                    type: 'FETCH_DATABASE_REJECTED',
-                    payload: err
-                })
-            })
-    })
-}
-
-// Register dispatcher every 10 minutes.
-const tenMinutes = 1000 * 60 * 10;
-setInterval(databaseDispatcher, tenMinutes);
-databaseDispatcher();
 
 export default store;
