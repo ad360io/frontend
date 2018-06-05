5a94d9a7bfbcb71c98568917e2dc322c6eb8dfb3
yao tsai <a9898123u@gmail.com>
2018-06-05 16:15:22 -0700

    form variable/label name changes

--- src/components/create-listing/CreateListingForm/CreateListingForm.component.js
+++ src/components/create-listing/CreateListingForm/CreateListingForm.component.js
@@ -14,6 +14,11 @@ Local CSS
 */
 import './CreateListingForm.component.css';
 
+/*
+Children Components
+*/
+import MarketingTypeDropdown from './MarketingTypeDropdown/MarketingTypeDropdown.component';
+
 /**
  * Create Listing Form Component
  */
@@ -22,55 +27,37 @@ class CreateListingForm extends Component {
     constructor(props){
         super(props);
         this.state = {
-            selectedGenre: 'Sponsorship',
-            pitch: '',
-            title: '',
-            subCategory: '',
+            description: '',
+            topic: '',
         }
 
-        this.handleTitleChange = this.handleTitleChange.bind(this);
-        this.handleOnGenreChange = this.handleOnGenreChange.bind(this);
-        this.handleSubCategoryChange = this.handleSubCategoryChange.bind(this);
-        this.handlePitchChange = this.handlePitchChange.bind(this);
+        this.handleTopicChange = this.handleTopicChange.bind(this);
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
 
-        this.decideTitle = this.decideTitle.bind(this);
-        this.showSubGenre = this.showSubGenre.bind(this);
+        this.decideFormTitle = this.decideFormTitle.bind(this);
+        this.decideDescriptionLabel = this.decideDescriptionLabel.bind(this);
         
         this.handleSubmitForm = this.handleSubmitForm.bind(this);
     }
 
-    decideTitle() {
+    decideFormTitle() {
         return (this.props.modeFilter === 'Advertiser' 
-            ? 'Content' 
-            : 'Content Space'
+            ? 'Request Content Space Availability'
+            : 'Create Content Space Listings'
         );
     }
 
-    handleOnGenreChange(event) {
+    handleDescriptionChange(event) {
         this.setState({
             ...this.state,
-            selectedGenre : event.target.value
-        });
-    }
-
-    handlePitchChange(event) {
-        this.setState({
-            ...this.state,
-            pitch: event.target.value
+            description: event.target.value
         })
     }
 
-    handleTitleChange(event) {
+    handleTopicChange(event) {
         this.setState({
             ...this.state,
-            title: event.target.value
-        })
-    }
-
-    handleSubCategoryChange(event) {
-        this.setState({
-            ...this.state,
-            subCategory: event.target.value
+            topic: event.target.value
         })
     }
 
@@ -78,91 +65,49 @@ class CreateListingForm extends Component {
         // build URL to Postgrest
     }
 
-    showSubGenre() {
-        if (this.state.selectedGenre === 'Branded Content') {
-            return <FormGroup controlId='control-form-genre'>
-                    <p className='control-label'>*Select Sub-Category </p>
-                    <FormControl 
-                        componentClass='select' 
-                        onChange={this.handleSubCategoryChange}
-                        required
-                    >
-                        <option value='Written Post'>Written Post</option>
-                        <option value='Podcast'>Podcast</option>
-                        <option value='Video'>Video</option>
-                        <option value='Other'>Other</option>
-                    </FormControl>
-            </FormGroup>
-        }else if (this.state.selectedGenre === 'Influencer Post'){
-            return <FormGroup controlId='control-form-genre'>
-                <p className='control-label'>*Select Sub-Category</p>
-                <FormControl 
-                    componentClass='select'
-                    onChange={this.handleSubCategoryChange}
-                    required
-                >
-                    <option value='Tweet'>Tweet</option>
-                    <option value='Instagram'>Instagram</option>
-                    <option value='Twitch'>Twitch</option>
-                    <option value='Youtube'>Youtube</option>
-                    <option value='Facebook'>Facebook</option>
-                    <option value='Twitter'>Twitter</option>
-                    <option value='Niconico'>Niconico</option>
-                    <option value='Other'>Other </option>
-                </FormControl>
-            </FormGroup>
-
-        }else {
-            return null;
-        }
+    decideDescriptionLabel() {
+        return (this.props.modeFilter === 'Advertiser' 
+                    ? "Content Description"
+                    : "Listing Description")
     }
-
 
     render() {
         return <div className='create-listing-form-container'>
-            <h2 className='create-listing-form-title'>Create an {this.decideTitle()} Listing</h2>
+            <h2 className='create-listing-form-title'>{this.decideFormTitle()}</h2>
 
             <form className='create-form'>
+                <MarketingTypeDropdown />
+
                 <FormGroup controlId='control-form-title'>
                     <p className='control-label'>
-                        {`*${this.decideTitle()} Title`}
+                        *Content Topic
                     </p>
-                    <FormControl type='text' onChange={this.handleTitleChange} required />
+                    <FormControl type='text' onChange={this.handleTopicChange} required />
                 </FormGroup>
-                <FormGroup controlId='control-form-genre'>
-                    <p className='control-label'>*Select Genre</p>
-                    <FormControl componentClass='select' onChange={this.handleOnGenreChange} required>
-                        <option value='Sponsorship'>Sponsorship</option>
-                        <option value='Branded Content'>Branded Content</option>
-                        <option value='Influencer Post'>Influencer Post</option>
-                    </FormControl>
+                
+                <FormGroup controlId='control-form-pitch'>
+                    <p className='control-label'>*{this.decideDescriptionLabel()} </p>
+                    <FormControl componentClass='textarea'  
+                        maxLength={280} 
+                        rows={8}
+                        style={{resize: 'vertical'}}
+                        onChange={this.handleDescriptionChange}    
+                        required
+                    />
                 </FormGroup>
-
-                {this.showSubGenre()}
-
                 <FormGroup controlId='control-form-image'>
                     <p className='control-label'>
-                        {`${this.decideTitle()} Image (optional)`}
+                        {/* {`${this.decideTitle()} Image (optional)`} */}
                     </p>
                     <FormControl type='file' />
                     <HelpBlock>Don't mess up your first impression!</HelpBlock>
                 </FormGroup>    
-                <FormGroup controlId='control-form-pitch'>
-                    <p className='control-label'>Pitch (optional)</p>
-                    <FormControl componentClass='textarea' 
-                        placeholder='Show me your best pitch here!' 
-                        maxLength={280} 
-                        rows={8}
-                        style={{resize: 'vertical'}}
-                        onChange={this.handlePitchChange}    
-                    />
-                </FormGroup>
                 <Button type='submit' 
                     className='control-form-submit btn-lg btn-primary'
                     onClick={this.handleSubmitForm}
                     disabled
                 >
-                    Create
+                    {this.props.modeFilter==='Publisher' ? 'Post Listing': 'Request Listing'}
                 </Button>
             </form>
         </div>;
---
+++ src/components/create-listing/CreateListingForm/MarketingTypeDropdown/MarketingTypeDropdown.component.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { FormGroup, FormControl } from 'react-bootstrap';
+
+class MarketingTypeDropdown extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedMedium: '',
+            selectedType: 'Sponsorship'
+        }
+        this.showMedium = this.showMedium.bind(this);
+        this.handleTypeChange = this.handleTypeChange.bind(this);
+        this.handleMediumChange = this.handleMediumChange.bind(this);
+    }
+
+    handleMediumChange (event) {
+        this.setState({
+            ...this.state,
+            selectedMedium: event.target.value
+        })            
+    }
+
+    handleTypeChange (event) {
+        this.setState({
+            ...this.state,
+            selectedType: event.target.value
+        })
+    }
+
+    showMedium() {
+        if (this.state.selectedType === 'Branded Content') {
+            return <FormGroup controlId='control-form-genre'>
+                    <p className='control-label'>*Select Sub-Category </p>
+                    <FormControl 
+                        componentClass='select' 
+                        onChange={this.handleMediumChange}
+                        required
+                    >
+                        <option value='Written Post'>Written Post</option>
+                        <option value='Podcast'>Podcast</option>
+                        <option value='Video'>Video</option>
+                        <option value='Other'>Other</option>
+                    </FormControl>
+            </FormGroup>
+        }else if (this.state.selectedType === 'Influencer Post'){
+            return <FormGroup controlId='control-form-genre'>
+                <p className='control-label'>*Select Sub-Category</p>
+                <FormControl 
+                    componentClass='select'
+                    onChange={this.handleMediumChange}
+                    required
+                >
+                    <option value='Tweet'>Tweet</option>
+                    <option value='Instagram'>Instagram</option>
+                    <option value='Twitch'>Twitch</option>
+                    <option value='Youtube'>Youtube</option>
+                    <option value='Facebook'>Facebook</option>
+                    <option value='Twitter'>Twitter</option>
+                    <option value='Niconico'>Niconico</option>
+                    <option value='Other'>Other </option>
+                </FormControl>
+            </FormGroup>
+
+        }else {
+            return null;
+        }
+    }
+
+    render () {
+        return <div>
+            <FormGroup controlId='control-form-genre'>
+                <p className='control-label'>*Select Authentic Marketing Type</p>
+                <FormControl componentClass='select' onChange={this.handleTypeChange} required>
+                    <option value='Sponsorship'>Sponsorship</option>
+                    <option value='Branded Content'>Branded Content</option>
+                    <option value='Influencer Post'>Influencer Post</option>
+                </FormControl>
+                </FormGroup>
+
+            {this.showMedium()}
+        </div>
+    }
+}
+
+
+export default MarketingTypeDropdown;
