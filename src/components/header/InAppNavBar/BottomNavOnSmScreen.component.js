d04e2be012c7176d9c6702a532244813386cea0e
yaotsai17 <yaotsai17@gmail.com>
2018-08-24 18:44:11 -0700

    bug fix: bottom nav didn't update correctly

--- src/components/header/InAppNavBar/BottomNavOnSmScreen.component.js
+++ src/components/header/InAppNavBar/BottomNavOnSmScreen.component.js
@@ -6,6 +6,7 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
+import { withWindowWidthListener } from '../../ResponsiveComponent/ResponsiveComponent';
 
 const styles = {
     root: {
@@ -21,32 +22,47 @@ class BottomNavOnSmScreen extends React.Component {
     constructor(props) {
         super(props);
         const { pathname } = this.props.history.location;
-        if(pathname === '/dashboard') {
+        if (pathname === '/dashboard') {
             this.state = {
                 value: 0
             }
-        }else if (pathname === '/marketplace') {
+        } else if (pathname === '/marketplace') {
             this.state = {
                 value: 1
             }
-        }else if (pathname === '/create') {
+        } else if (pathname === '/create') {
             this.state = {
                 value: 2
             }
-        }else {
+        } else {
             this.state = {
                 value: undefined
             }
         }
     }
-    
+
+    componentWillUpdate(prevProps) {
+        if (prevProps.width !== this.props.width) {
+            const { pathname } = this.props.history.location;
+            if (pathname === '/dashboard') {
+                this.setState({ ...this.state, value: 0 })
+            } else if (pathname === '/marketplace') {
+                this.setState({ ...this.state, value: 1 })
+            } else if (pathname === '/create') {
+                this.setState({ ...this.state, value: 2 })
+            } else {
+                this.setState({ ...this.state, value: undefined })
+            }
+        }
+    }
+
     handleChange = (event, value) => {
         this.setState({ value });
-        if(value === 0) {
+        if (value === 0) {
             this.props.history.push('/dashboard')
-        }else if (value === 1) {
+        } else if (value === 1) {
             this.props.history.push('/marketplace')
-        }else if (value === 2) {
+        } else if (value === 2) {
             this.props.history.push('/create')
         }
     };
@@ -54,7 +70,7 @@ class BottomNavOnSmScreen extends React.Component {
     render() {
         const { classes } = this.props;
         const { value } = this.state;
-        
+
         if (this.props.width <= 768) {
             return (
                 <BottomNavigation
@@ -63,9 +79,9 @@ class BottomNavOnSmScreen extends React.Component {
                     showLabels
                     className={classes.root}
                 >
-                    <BottomNavigationAction style={(value === 0) ? { color: 'rgb(145,53,161)' } : { color: '#777'}} label="Dashboard" icon={<DashboardIcon />} />
-                    <BottomNavigationAction style={(value === 1) ? { color: 'rgb(145,53,161)' } : { color: '#777'}} label="Marketplace" icon={<ShoppingCartIcon />} />
-                    <BottomNavigationAction style={(value === 2) ? { color: 'rgb(145,53,161)' } : { color: '#777'}} label={this.props.modeFilter === 'Advertiser' ? 'Request' : 'Create'} icon={<PlaylistAddIcon />} />
+                    <BottomNavigationAction style={(value === 0) ? { color: 'rgb(145,53,161)' } : { color: '#777' }} label="Dashboard" icon={<DashboardIcon />} />
+                    <BottomNavigationAction style={(value === 1) ? { color: 'rgb(145,53,161)' } : { color: '#777' }} label="Marketplace" icon={<ShoppingCartIcon />} />
+                    <BottomNavigationAction style={(value === 2) ? { color: 'rgb(145,53,161)' } : { color: '#777' }} label={this.props.modeFilter === 'Advertiser' ? 'Request' : 'Create'} icon={<PlaylistAddIcon />} />
                 </BottomNavigation>
             );
         } else {
@@ -80,4 +96,4 @@ BottomNavOnSmScreen.propTypes = {
 };
 
 
-export default withStyles(styles)(BottomNavOnSmScreen);
+export default withWindowWidthListener(withStyles(styles)(BottomNavOnSmScreen));
